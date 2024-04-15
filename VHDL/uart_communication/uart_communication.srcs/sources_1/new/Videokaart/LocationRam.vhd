164622a8a2516236a09c9e9552e0efafdd5f78c9
Jasper Verplanke <146102893+jasperverplanke@users.noreply.github.com>
2024-04-15 21:49:43 +0200

    Update LocationRam.vhd comments

--- VHDL/uart_communication/uart_communication.srcs/sources_1/new/Videokaart/LocationRam.vhd
+++ VHDL/uart_communication/uart_communication.srcs/sources_1/new/Videokaart/LocationRam.vhd
@@ -1,6 +1,6 @@
 ----------------------------------------------------------------------------------
 -- Company: 
--- Engineer: 
+-- Engineer: Jasper Verplanke
 -- 
 -- Create Date: 08.03.2024 11:21:42
 -- Design Name: 
@@ -49,8 +49,9 @@ end locationRAM;
 architecture Behavioral of locationRAM is
 
 
-TYPE ramType is ARRAY (2**addr_length-1 downto 0)
-        of std_logic_vector(data_width-1 downto 0);
+TYPE ramType is ARRAY (2**addr_length-1 downto 0)    --array row zise for robots and bullets(usually  127 downto 0)
+        of std_logic_vector(data_width-1 downto 0); -- set the sprite ID size (usually 3 downto 0)
+
 signal ram: ramType := (others => (others => '0'));
 
 signal oldEn : std_logic := '0';
@@ -62,16 +63,16 @@ begin
 
     if(rising_edge (clk))then 
         
-        if (oldEn = '1' and outputEn = '0')then 
+        if (oldEn = '1' and outputEn = '0')then  -- wipe the entire row after it has been read/ when the ram returns to write-mode
             ram <= (others => (others => '0'));
         end if;
     
     
-        if (outputEn = '0')then 
+        if (outputEn = '0')then                     --write
             ram(to_integer(unsigned(Addr))) <= create;
         end if;    
         
-        if (outputEn = '1')then
+        if (outputEn = '1')then                        --read
             data <= ram(TO_INTEGER(UNSIGNED(Addr)));     
         end if;
         oldEn <= outputEN;
