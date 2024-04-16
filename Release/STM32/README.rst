f376bb386be50a7e0625b454abff16f3636783e8
Kasper <kasperjnssen@gmail.com>
2024-04-16 15:15:34 +0200

    Delete Release directory

--- Release/STM32/README.rst
+++
@@ -1,2 +0,0 @@
-Uart toegevoegd aan de button sample
-Kan een led aan en uit zetten op een fpga met de bijhoorende vhdl code
--- Release/STM32/header/menu.h
+++
@@ -1,8 +0,0 @@
-#ifndef MENU_H_
-#define MENU_H_
-
-#include"main.h"
-
-void deadscreen(struct Player* player);
-
-#endif //MENU_H_
